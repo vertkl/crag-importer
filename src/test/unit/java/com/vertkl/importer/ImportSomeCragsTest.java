7c628d7f4e4f959d70788d7265f573ff9c45ca52
Jim Barritt <jim@planet-ix.com>
2011-10-30 14:23:48 +0000

    [jmdb] Got the import of a crag to work

--- src/test/unit/java/com/vertkl/importer/ImportSomeCragsTest.java
+++ src/test/unit/java/com/vertkl/importer/ImportSomeCragsTest.java
@@ -3,10 +3,12 @@ package com.vertkl.importer;
 import ixcode.platform.http.client.Http;
 import ixcode.platform.http.representation.Representation;
 import org.apache.log4j.Logger;
+import org.htmlcleaner.ContentNode;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.TagNode;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static ixcode.platform.http.protocol.UriFactory.uri;
@@ -27,16 +29,44 @@ public class ImportSomeCragsTest {
 
         TagNode root = new HtmlCleaner().clean(representation.<String>getEntity());
         TagNode h1 = root.findElementByName("h1", true);
-        String cragName = h1.getChildren().get(0).toString();
+        String areaName = h1.getChildren().get(0).toString();
+
+        List<String> routes = new ArrayList<String>();
 
         List<TagNode> links = root.getElementListByName("a", true);
         for (TagNode node : links) {
-            log.debug(node.getAttributeByName("href"));
+            String href = node.getAttributeByName("href");
+            if (href.startsWith("c.php?")) {
+                String name = node.getChildren().get(0).toString();
+
+                String grade = null;
+                if (node.getParent().getChildren().size() > 2) {
+                    TagNode gradeNode = (TagNode) node.getParent().getParent().getChildren().get(2);
+                    TagNode fontNode = (TagNode) gradeNode.getChildren().get(0);
+                    ContentNode contentNode = (ContentNode)fontNode.getChildren().get(0);
+                    grade = contentNode.toString().trim();
+                }
+
+                if (grade == null) {
+                    TagNode gradeNode = (TagNode) node.getParent().getParent().getChildren().get(2);
+                    ContentNode contentNode = (ContentNode)gradeNode.getChildren().get(0);
+                    grade = contentNode.toString().trim();
+                }
+                String id = href.substring(8);
+                routes.add(format("%s : %s : %s", name, grade, id));
+            }
+            //log.debug(node.getAttributeByName("href") + " :  " + node.getAttributeByName("rel") + " : " + node.getChildren().get(0));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        for (String route : routes) {
+            sb.append(route).append("\n");
         }
 
         log.debug("Crag Id   : " + 20);
-        log.debug("Crag Name : " + cragName);
-
+        log.debug("Crag Name : " + areaName);
+        log.debug("Routes    : " + sb);
 
     }
 
